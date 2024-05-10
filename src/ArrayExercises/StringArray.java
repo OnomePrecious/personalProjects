1091c3e441c2578593d9809a58dbb30d9ac0c5c2
Precious <preciousonome002@gmail.com>
2024-05-10 21:48:52 +0100

    Java--Assignment

---
+++ src/ArrayExercises/StringArray.java
@@ -0,0 +1,11 @@
+package ArrayExercises;
+
+public class StringArray {
+    public static void main(String[] args) {
+        String [] array = {"""
+Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday"""};
+        for(String result: array){
+            System.out.println(result);
+        }
+    }
+}
