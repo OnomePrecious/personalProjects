9ce1d6c14ab8a1a247f0dd4506da7541634a5b62
Precious <preciousonome002@gmail.com>
2024-05-17 13:29:36 +0100

    Java--Assignment

--- src/myStack/MyStack.java
+++ src/myStack/MyStack.java
@@ -2,9 +2,12 @@ package myStack;
 
 import java.util.ArrayList;
 
-public class PractiseStack {
+public class MyStack {
     private static ArrayList<Integer> numbers = new ArrayList<>();
     private static int size;
+
+    public MyStack(int size) {
+    }
 
     public static int pop() {
         int number = numbers.get(numbers.size() - 1);
@@ -14,6 +17,12 @@ public class PractiseStack {
     }
     public static void push(int number) {
         numbers.add(number);
-        size++;
+    }
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public static boolean isFull() {
+        return size == numbers.size();
     }
 }
